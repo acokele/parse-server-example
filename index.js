289775c5cec6231e7316cd372623282a50929c3e
Aleksandar Kelecevic <acokele@gmail.com>
2017-06-22 10:25:25 +0200

    Configured push (hopefully)

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  }, 
+  push: {
+    ios: {
+      pfx: 'cert.p12',
+      passphrase: 'sigurnost', // optional password to your p12/PFX
+      bundleId: 'io.arcesio.Arcesio',
+      production: true
+    }
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
